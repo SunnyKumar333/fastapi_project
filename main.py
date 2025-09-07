d802631a7e9e9fb8730c345be3d8d2786f22505d
SunnyKumar333 <tk2242906@gmail.com>
2025-09-07 22:32:38 +0530

    refactor project folder structure

--- main.py
+++ main.py
@@ -1,38 +1,14 @@
 from fastapi import FastAPI,status
-from fastapi.exceptions import HTTPException
-from typing import Optional,List
-from pydantic import BaseModel
-from books import books
-from datetime import datetime
+from routers.book_router import book_router
 
 
-app = FastAPI()
 
-class BookCreateModel(BaseModel):
-    title: str
-    author: str
-
-@app.get("/books",status_code=200,response_model=List[BookCreateModel])
-async def get_all_books()-> dict:
-    return books
-
-@app.get("/books/{book_id}",response_model=BookCreateModel)
-async def greet(book_id:str)->dict:
-    book= list(filter(lambda book:book["id"]==book_id,books))
-    if len(book)>0:
-        return book[0]
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Book not found with id: {book_id}")
-
-
-@app.post("/books",status_code=status.HTTP_201_CREATED)
-async def create_book(book_data: BookCreateModel):
-    books.append({
-        "id":str(int(books[-1]["id"])+1),
-        "title":book_data.title,
-        "author": book_data.author,
-        "created_at":datetime.now()
-    })
-
-    return {
-        "success":True
-    }
+app = FastAPI(
+    title="Bookily",
+    description="RSST api services for book review services",
+    version="v1"
+)
+@app.get("/health")
+async def get_health():
+    return {"status":"OK"}
+app.include_router(book_router,prefix="/api/v1/books",tags=["books api list"])
---
+++ routers/book_router.py
@@ -0,0 +1,40 @@
+from fastapi import APIRouter,status
+from fastapi.exceptions import HTTPException
+from typing import Optional,List
+from pydantic import BaseModel
+from models.book_data import books
+from datetime import datetime
+print("*"*100)
+print(books)
+print("*"*100)
+
+book_router = APIRouter()
+
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+@book_router.get("/",status_code=200,response_model=List[BookCreateModel])
+async def get_all_books()-> dict:
+    return books
+
+@book_router.get("/{book_id}",response_model=BookCreateModel)
+async def greet(book_id:str)->dict:
+    book= list(filter(lambda book:book["id"]==book_id,books))
+    if len(book)>0:
+        return book[0]
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Book not found with id: {book_id}")
+
+
+@book_router.post("/",status_code=status.HTTP_201_CREATED)
+async def create_book(book_data: BookCreateModel):
+    books.append({
+        "id":str(int(books[-1]["id"])+1),
+        "title":book_data.title,
+        "author": book_data.author,
+        "created_at":datetime.now()
+    })
+
+    return {
+        "success":True
+    }
